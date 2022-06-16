28eed6d6b8246b455f6f5d6fa9869f7d1207090b
IvanShmytov <ivanshmytov@mail.ru>
2022-06-16 15:33:56 +0300

    Добавьте файлы проекта.

---
+++ Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace UserTicketService
+{
+    public class Calculator
+    {
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
